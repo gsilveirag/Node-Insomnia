b11f43ee882d07bd6e66f8268fae5c6474a0149b
Sala 18A <sala18a@ceines.com.br>
2023-06-20 21:10:32 -0300

    terceiro commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-app.use(express.json())
+const cors = require('cors')
+
 const pool = require('./db')
 
+app.use(express.json())
+app.use(cors())
 
 app.get('/pegar/usuario', async function (req, res) {
     try{
